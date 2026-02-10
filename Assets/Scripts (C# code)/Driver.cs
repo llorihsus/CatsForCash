9025fba9907cfb51d819ad5c82bdaaf7ca67230d
llorihsus <130130292+llorihsus@users.noreply.github.com>
2026-02-10 01:57:21 -0800

    Added Neighborhood prefab

--- Assets/Scripts (C# code)/Driver.cs
+++ Assets/Scripts (C# code)/Driver.cs
@@ -6,11 +6,10 @@ public class Driver : MonoBehaviour
 {
     // class-level variables
     [Header("Movement Settings")]
-    public float currentSpeed = 7f; // Visible in Inspector
-    private float steerSpeed = 300f; // Hidden in Inspector, private is default when no access modifier is specified
-    [SerializeField] float rotationSpeed = 50f;
-    [SerializeField] float regularSpeed = 5f;
-    [SerializeField] float slowSpeed = 2f;
+    [SerializeField] float currentSpeed = 1f; // Visible in Inspector
+    [SerializeField] float regularSpeed = 1f;
+    [SerializeField] float slowSpeed = 0.5f;
+    [SerializeField] float steerSpeed = 25f;
 
     [Header("Counters")]
     [SerializeField] int numberOfDeliveries = 0; // [SerializeField] makes private variables visible in Inspector while keeping them private in code
@@ -19,25 +18,11 @@ public class Driver : MonoBehaviour
     // Collision Test
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.name == "Wall")
-        {
-            Debug.Log("Hit a wall!");
-            currentSpeed = slowSpeed; // Slow down when hitting a wall
-        }
-        else if (collision.gameObject.name == "Rock")
-        {
-            Debug.Log("Hit a rock!");
-            currentSpeed = slowSpeed; // Slow down when hitting a rock
-        }
-
-       /* if (collision.collider.CompareTag("WorldCollision"))
+        if (collision.collider.CompareTag("WorldCollision"))
         {
             currentSpeed = regularSpeed;
-            boostText.gameObject.setActive(false);
-        Challenge:
-         Arraylist of hte packages and showing the path to the requester 
-
-        } */
+            //boostText.gameObject.setActive(false);
+        }
     }
 
     //Trigger Test
@@ -50,12 +35,12 @@ public class Driver : MonoBehaviour
             currentSpeed = regularSpeed; // Restore speed after picking up package
         }
 
-        /* if (collision.CompareTag("Boost"))
+        if (collision.CompareTag("Boost"))
         {
-            currentSpeed = boostSpeed;
-            boostText.gameObject.SetActive(true);
+            //increase timer 
+            //boostText.gameObject.SetActive(true);
             Destroy(collision.gameObject);
-        } */
+        } 
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -67,37 +52,34 @@ public class Driver : MonoBehaviour
     // Update is called once per frame (60+ times per second)
     void Update()
     {
-        /* transform.Translate(moveSpeed, 0.01f, 0);    Translate(): Moves an object by adding to its current position in (x,y,z)
-                                                        Unity's transform.Translate() expects floats(32-bit), not doubles(64-bit). In C# decimal numbers are treated as double by default. */
-
         float move = 0f;
         float steer = 0f;
 
         // Forward/Backward movement
         if (Keyboard.current.wKey.isPressed)
         {
-            move = 1f;
+            move = -0.15f;
         }
         else if (Keyboard.current.sKey.isPressed)
         {
-            move = -1f;
+            move = 0.15f;
         }
 
         // Left/Right rotation
         if (Keyboard.current.aKey.isPressed)
         {
-            steer = 1f;
+            steer = 0.75f;
         }
         else if (Keyboard.current.dKey.isPressed)
         {
-            steer = -1f;
+            steer = -0.75f;
         }
 
         // Apply movement
         float moveAmount = move * currentSpeed * Time.deltaTime;    // Time.deltaTime makes movement frame rate independent
         float steerAmount = steer * steerSpeed * Time.deltaTime; // Speed is now units per second rather than units pers frame
 
-        transform.Translate(0, moveAmount, 0);
+        transform.Translate(moveAmount, 0, 0);
         transform.Rotate(0, 0, steerAmount);
 
     }
